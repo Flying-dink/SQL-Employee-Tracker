92a167da70b89c3639d2b813e2a2afe21d56568f
gail enstice <skyedoll@aol.com>
2021-09-04 23:48:07 -0400

    Finished Tables

--- seeds.sql
+++ seeds.sql
@@ -3,6 +3,7 @@ SELECT role.id, role.title FROM role ORDER BY role.id;
 SELECT * FROM employee;
 
 SELECT department.id, department.name FROM department ORDER BY department.id;
+
 SELECT deaprtment.name AS department, role.title, employee id, employee.first_name, employee.last_name
 FROM employee 
 LEFT JOIN role ON ( role.id = employee.role_id)
@@ -19,8 +20,14 @@ ORDER BY manager;
 SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
 FROM employee
 LEFT JOIN role ON(role.id = employee.role.id = role. department_id)
+LEFT JOIN department ON (department.id = role.department_id)
 ORDER BY role.title;
 
 SELECT employee.id, employee.first_name, employee.last_name, role title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
 FROM employee
 LEFT JOIN employee manager on manager.id = employee.manager_id
+INNER JOIN role one (role.id = employee.role_id)
+INNER JOIN department ON (department.id = role.department_id)
+ORDER BY employee.id; 
+
+SELECT first_name, last_name role_id FROM employee  WHERE employee.id = 4;

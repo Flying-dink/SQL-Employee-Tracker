237d6885a08a469d6363bbbd5a4b237fe74e09f2
gail enstice <skyedoll@aol.com>
2021-10-03 14:43:38 -0400

    changed tables

--- schema.sql
+++ schema.sql
@@ -4,37 +4,64 @@ DROP DATABASE IF EXISTS employeegroup;
 CREATE DATABASE employeegroup;
 USE employeegroup;
 
-
-DROP TABLE IF  EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
-
-
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45)NULL,
+    `name` VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-
 );
 
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR (45) NULL,
-    salary DECIMAL (10.3) NULL,
-    department_id INT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE `role` (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(50,2) NOT NULL,
+    departmentid INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (departmentid) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30)NOT NULL,
-    last_name VARCHAR (30)NOT NULL,
-    role_id INT ,
-    manager_id INT,
-    PRIMARY KEY (id)
-    
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roleid INT,
+    manageid INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manageid) REFERENCES employee(id),
+    FOREIGN KEY (roleid) REFERENCES role(id) ON DELETE CASCADE
 );
-use employeegroup ;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+INSERT INTO role
+    (title, salary, departmentid)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+INSERT INTO employee
+    (first_name, last_name, roleid, managerid)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
+
+
+
 
 
 
